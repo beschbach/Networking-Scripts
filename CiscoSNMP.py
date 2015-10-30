8c172f941118c2b458e2c1f92f8c17c45c4265b0
beschbach <brett.eschbach@gmail.com>
2015-10-29 21:54:03 -0500

    Scripts

---
+++ CiscoSNMP.py
@@ -0,0 +1,50 @@
+#Code adds a new SNMP RO community string
+
+import pexpect
+import sys
+import os
+import telnetlib
+
+# Variables to be referenced by code
+router_usrn = "cisco"
+router_pwd = "cisco"
+enable_pwd = "cisco"
+
+# Creat a logfile
+stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+# Import flat file of Router IP's and loop to modify SNMP string
+
+with open('devices.txt') as filename:
+    routers = filename.read().splitlines()
+for router in routers:
+	print router
+	task = pexpect.spawn('telnet %s' % (router))
+	# Drop logfile to Standard Out.
+	task.logfile = stdout
+	task.expect('Password:')
+	task.sendline(router_pwd)
+	task.expect('>')
+	task.sendline('terminal length 0')
+	task.expect('\>')
+	task.sendline('enable')
+	task.expect('Password:')
+	task.sendline(enable_pwd)
+	task.expect('#')
+	task.sendline('conf t')
+	
+	# This section starts the configuration of SNMP 
+	task.expect('\(config\)#')
+	task.sendline('snmp-server community TEST ro')
+	task.expect('\(config\)#')
+	task.sendline('exit')
+	task.expect('#')
+	
+	#terminate the session
+	
+	task.sendline('end')
+	task.expect('#')
+	task.sendline('wr mem')
+	task.expect('#')
+	task.sendline('quit')
+	task.close()
